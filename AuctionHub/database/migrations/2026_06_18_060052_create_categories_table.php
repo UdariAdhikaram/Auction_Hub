3bcabc921b98e6974fe79a15c7895c5799082d75
Udari Adhikaram <adhi.uda98@gmail.com>
2026-06-18 13:59:27 +0530

    create category table

--- AuctionHub/database/migrations/2026_06_18_060052_create_categories_table.php
+++ AuctionHub/database/migrations/2026_06_18_060052_create_categories_table.php
@@ -10,12 +10,14 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('categories', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->foreignId('parent_id')->nullable()->constrained('categories')->onDelete('cascade');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
