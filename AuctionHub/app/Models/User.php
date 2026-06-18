db9f81589a8d370d4e4ca25975e5b8763e02b290
Udari Adhikaram <adhi.uda98@gmail.com>
2026-06-18 13:10:58 +0530

    create user model

--- AuctionHub/app/Models/User.php
+++ AuctionHub/app/Models/User.php
@@ -2,48 +2,64 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role',
+        'kyc_verified_at',
+        'deposit_balance',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'kyc_verified_at' => 'datetime',
+        'deposit_balance' => 'decimal:2',
+    ];
+
+    // Relationships
+    public function vendor()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasOne(Vendor::class);
+    }
+
+    public function auctions()
+    {
+        return $this->hasMany(Auction::class, 'vendor_id');
+    }
+
+    public function bids()
+    {
+        return $this->hasMany(Bid::class);
+    }
+
+    public function watchlists()
+    {
+        return $this->belongsToMany(Auction::class, 'watchlists')
+                    ->withPivot('notify_at_close')
+                    ->withTimestamps();
+    }
+
+    // Scopes
+    public function scopeWithActiveBidCount($query)
+    {
+        return $query->withCount(['bids' => function ($query) {
+            $query->whereHas('auction', function ($q) {
+                $q->where('status', 'live');
+            });
+        }]);
     }
 }
